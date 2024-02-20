a879a07c2a52bbd35975ed0722b1ae7d435deb41
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2024-02-21 01:17:17 +0530

    micronaut

---
+++ mn-start/src/test/java/com/milton/lseg/MnStartTest.java
@@ -0,0 +1,35 @@
+package com.milton.lseg;
+
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+class HelloWorldControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient client;
+
+    @Test
+    void helloWorldEndpointTest() {
+        var response = client.toBlocking().retrieve("/hello");
+        assertEquals("Hello World!", response);
+    }
+
+    @Test
+    void helloWorldEndPointRespondWithProperStatusCodeAndContext() {
+        var response = client.toBlocking().exchange("/hello", String.class);
+        assertEquals("Hello World!", response.getBody().get());
+        assertEquals(HttpStatus.OK, response.getStatus());;
+    }
+
+}
